a0c986308ee25dd4a028abcfed9eb5b9ae58cb9c
FaultyTwo <mapcast42@gmail.com>
2024-06-25 16:02:29 +0700

    starting to add info

--- res/index.js
+++ res/index.js
@@ -1,10 +1,13 @@
+
+let memo = "res/subpages/greeting.html"; // stale
 function changePage(page_name){
     // let's assume that resource always exist and dom can be loaded properly
     // im too lazy to ajax it
     let resource = `res/subpages/${page_name}.html`;
     let dom = document.getElementById("info-iframe");
-    if(dom === null){
+    if(dom === null || memo == resource){
         return;
     }
+    memo = resource;
     dom.src = resource;
 }
